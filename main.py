4f5ac7ea5838cdfdd16553654b58c3beda22c83d
sondog <sjh10271@naver.com>
2022-01-11 12:40:17 +0900

    fix model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Importing the library
 
 import os
+import sys
 
 import pandas as pd
 import numpy as np
@@ -12,6 +13,11 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2, f_regression, mutual_info_regression
+
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
 
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import GridSearchCV
@@ -42,12 +48,8 @@ target_list = [
     "zmag_RAW_IMU",
     "vibration_x_VIBRATION",
     "vibration_y_VIBRATION",
-    "vibration_z_VIBRATION",
+    "vibration_z_VIBRATION"
 ]
-
-feature_list = os.listdir(
-    "/home/aiteam/son/AI-voucher-Smatii/result/FeatureSelection/220106"
-)
 
 def evaluation(y_hat, predictions):
     mae = mean_absolute_error(y_hat, predictions)
@@ -57,24 +59,31 @@ def evaluation(y_hat, predictions):
     return mae, mse, rmse, r_squared
 
 
-def feature_selection(x, y , k , sf = f_regression):
+def feature_selection(x, y, k, sf=f_regression):
     skb = SelectKBest(score_func=sf, k=k)
     fit = skb.fit(x, y)
 
     dfscores = pd.DataFrame(fit.scores_)
     dfcolumns = pd.DataFrame(x.columns)
     featureScores = pd.concat([dfcolumns, dfscores], axis=1)
-    featureScores.columns = ['Specs', 'Score']  # naming the dataframe columns
-    best_feature = list(featureScores.nlargest(k, 'Score').iloc[:,0])  # get k best columns to LIST
+    featureScores.columns = ["Specs", "Score"]  # naming the dataframe columns
+    best_feature = list(
+        featureScores.nlargest(k, "Score").iloc[:, 0]
+    )  # get k best columns to LIST
 
     return best_feature
 
 
-def get_output(result:pd.DataFrame, model , target, k):
-    dir_name = 'result/'
-    file_name = f'SMATII_{model}_{target}_{k}'
-    result.to_csv(dir_name + file_name , encoding='CP949', index=False)
+def make_output_data(result: pd.DataFrame, model, target):
+    dir_name = "result/"
+    file_name = f"SMT_out_{model}_{target}_mm_220111"
+    result.to_csv(dir_name + file_name + ".csv", encoding="CP949", index=False)
     pass
+
+
+def make_output_metric(y_true, y_pred):
+    mae, mse, rmse, r2 = evaluation(y_true, y_pred)
+    return
 
 
 def xgb(
@@ -99,71 +108,109 @@ def xgb(
     # )
 
     xgb_model = xgboost.XGBRegressor()
-    param_grid = {'booster': ['gbtree'],
-                  'max_depth': [5, 6, 8],
-                  'min_child_weight': [1, 3, 5],
-                  'gamma': [0, 1, 2, 3],
-                  'nthread': [4],
-                  'colsample_bytree': [0.5, 0.8],
-                  'colsample_bylevel': [0.9],
-                  'n_estimators': [50],
-                  'random_state': [2]}
 
-    gcv = GridSearchCV(xgb_model, param_grid=param_grid, cv=5, scoring='neg_root_mean_squared_error', n_jobs=4)
+    param_grid = {
+        "booster": ["gbtree"],
+        "max_depth": [5, 6, 8],
+        "min_child_weight": [1, 3, 5],
+        "gamma": [0, 1, 2, 3],
+        "nthread": [4],
+        "colsample_bytree": [0.5, 0.8],
+        "colsample_bylevel": [0.9],
+        "n_estimators": [50],
+        "random_state": [2],
+    }
+
+    # gcv = GridSearchCV(
+    #     xgb_model,
+    #     cv=5,
+    #     scoring="neg_root_mean_squared_error",
+    #     n_jobs=4,
+    # )
+
+    # split
+    train_x, test_x, train_y, test_y = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
     # fitting
-    gcv.fit(X_train, y_train)
-
-    print('best params', gcv.best_params_)  # best param
-    print('best score', gcv.best_score_)  # best score
-
+    xgb_model.fit(train_x, train_y)
+    #
+    # print("best params", gcv.best_params_)  # best param
+    # print("best score", gcv.best_score_)  # best score
 
     # predict by dataset
     if predict_all == False:
-        y_pred = cross_val_predict(gcv, X_train, y_train, cv=5)
+        y_pred = cross_val_predict(xgb_model, X_train, y_train, cv=5)
         y_true = y_train
         result = X_train.copy()
 
     elif predict_all == True:
-        y_pred = cross_val_predict(gcv, X, y, cv=5)
+        y_pred = cross_val_predict(xgb_model, X, y, cv=5)
         y_true = y
         result = X.copy()
 
-
     result[f"{target}_true"] = y_true
-    result[f"{target}pred"] = y_pred
+    result[f"{target}_pred"] = y_pred
 
-    return result
+    return result, y_true, y_pred
 
 
 if __name__ == "__main__":
 
-    # read_csv
-    df = pd.read_csv('/home/aiteam/son/AI-voucher-Smatii/data/normal_dataset.csv')
+    model = "xgb"
 
-    # set target col
-    target = target_list[0]
+    output_metric = pd.DataFrame(
+        columns=["TARGET", "MAE", "MSE", "RMSE", "R_SQUARE"]
+    )
+    dicts = {}
 
-    # make dataset
-    x = df.drop(target , axis = 1)
-    y = df[target]
+    for tg in target_list:
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"TARGET:{tg}, Start at {current_time}")
+        target = tg
 
-    # feature selection
-    cols = feature_selection(x=x , y=y , k=10)
-    x = x[cols]
-    print(x)
+        # read_csv
+        df = pd.read_csv(
+            "/home/aiteam/son/AI-voucher-Smatii/data/outlier_dataset.csv"
+        )
 
-    # run xgb
-    """
-    X_train
-    y_train
-    predict_all=False
-    X=None
-    y=None"""
+        # make dataset
+        x = df.drop(target, axis=1)
+        y = df[target]
 
-    result = xgb(X_train=x , y_train=y)
-    get_output(result)
+        std_scaler = MinMaxScaler()
+        std_scaler.fit(x)
 
-    print(result)
+        x_scale_trans = std_scaler.transform(x)
 
-    pass
+        scaled_x = pd.DataFrame(x_scale_trans, columns=x.columns)
+
+
+
+        # feature selection
+        # cols = feature_selection(x=x, y=y, k=k)
+        # x = x[cols]
+
+        # run model
+        result, y_true, y_pred = xgb(X_train=scaled_x, y_train=y)
+
+        # make output prediction
+        make_output_data(result, "xgb", target)
+
+        # eval & make output eval
+        mae, mse, rmse, r2 = evaluation(y_true, y_pred)
+        dicts.update(
+            {
+                "TARGET": target,
+                "MAE": mae,
+                "MSE": mse,
+                "RMSE": rmse,
+                "R_SQUARE": r2,
+            }
+        )
+        output_metric = output_metric.append(dicts, True)
+        print(output_metric)
+
+        pass
+
+    output_metric.to_csv("SMT_out_XGB_prediction_mm_220111.csv", encoding="CP949", index=False)
