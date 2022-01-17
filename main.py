8230fc173e80f7acaec1058733ebf830ce654b99
sondog <sjh10271@naver.com>
2022-01-17 11:17:46 +0900

    refactory model

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import numpy as np
 from datetime import datetime
 
 import xgboost
+from sklearn.linear_model import Ridge
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+
 
 from sklearn.model_selection import train_test_split
 
@@ -33,6 +37,9 @@ from sklearn.metrics import (
 )
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import Ridge
+
 target_list = [
     "roll_ATTITUDE",
     "pitch_ATTITUDE",
@@ -48,8 +55,63 @@ target_list = [
     "zmag_RAW_IMU",
     "vibration_x_VIBRATION",
     "vibration_y_VIBRATION",
-    "vibration_z_VIBRATION"
+    "vibration_z_VIBRATION",
 ]
+
+col_list = [
+    "roll_ATTITUDE",
+    "pitch_ATTITUDE",
+    "yaw_ATTITUDE",
+    "error_rp_AHRS",
+    "error_yaw_AHRS",
+    "omegaIx_AHRS",
+    "omegaIy_AHRS",
+    "omegaIz_AHRS",
+    "roll_AHRS2",
+    "pitch_AHRS2",
+    "yaw_AHRS2",
+    "roll_AHRS3",
+    "pitch_AHRS3",
+    "yaw_AHRS3",
+    "velocity_variance_EKF_STATUS_REPORT",
+    "pos_horiz_variance_EKF_STATUS_REPORT",
+    "pos_vert_variance_EKF_STATUS_REPORT",
+    "compass_variance_EKF_STATUS_REPORT",
+    "nav_roll_NAV_CONTROLLER_OUTPUT",
+    "nav_pitch_NAV_CONTROLLER_OUTPUT",
+    "xtrack_error_NAV_CONTROLLER_OUTPUT",
+    "aspd_error_NAV_CONTROLLER_OUTPUT",
+    "xacc_RAW_IMU",
+    "yacc_RAW_IMU",
+    "zacc_RAW_IMU",
+    "xgyro_RAW_IMU",
+    "ygyro_RAW_IMU",
+    "zgyro_RAW_IMU",
+    "xmag_RAW_IMU",
+    "ymag_RAW_IMU",
+    "zmag_RAW_IMU",
+    "xacc_SCALED_IMU2",
+    "yacc_SCALED_IMU2",
+    "zacc_SCALED_IMU2",
+    "xgyro_SCALED_IMU2",
+    "ygyro_SCALED_IMU2",
+    "zgyro_SCALED_IMU2",
+    "xmag_SCALED_IMU2",
+    "ymag_SCALED_IMU2",
+    "zmag_SCALED_IMU2",
+    "servo1_raw_SERVO_OUTPUT_RAW",
+    "servo2_raw_SERVO_OUTPUT_RAW",
+    "servo3_raw_SERVO_OUTPUT_RAW",
+    "servo4_raw_SERVO_OUTPUT_RAW",
+    "servo5_raw_SERVO_OUTPUT_RAW",
+    "servo6_raw_SERVO_OUTPUT_RAW",
+    "servo7_raw_SERVO_OUTPUT_RAW",
+    "servo8_raw_SERVO_OUTPUT_RAW",
+    "vibration_x_VIBRATION",
+    "vibration_y_VIBRATION",
+    "vibration_z_VIBRATION",
+]
+
 
 def evaluation(y_hat, predictions):
     mae = mean_absolute_error(y_hat, predictions)
@@ -74,9 +136,9 @@ def feature_selection(x, y, k, sf=f_regression):
     return best_feature
 
 
-def make_output_data(result: pd.DataFrame, model, target):
-    dir_name = "result/"
-    file_name = f"SMT_out_{model}_{target}_mm_220111"
+def make_output_data(result: pd.DataFrame, model, target, path):
+    dir_name = "result/220111/cross_test/data/"
+    file_name = f"SMT_out_{model}_{target}_rfe_std_cross_220113"
     result.to_csv(dir_name + file_name + ".csv", encoding="CP949", index=False)
     pass
 
@@ -86,65 +148,107 @@ def make_output_metric(y_true, y_pred):
     return
 
 
-def xgb(
+def fix_columns(df: pd.DataFrame):
+    df["roll_AHRS_DIFF"] = df["roll_AHRS2"] - df["roll_AHRS3"]
+    df["pitch_AHRS_DIFF"] = df["pitch_AHRS2"] - df["pitch_AHRS3"]
+    df["yaw_AHRS_DIFF"] = df["yaw_AHRS2"] - df["yaw_AHRS3"]
+    df["xacc_IMU_DIFF"] = df["xacc_RAW_IMU"] - df["xacc_SCALED_IMU2"]
+    df["yacc_IMU_DIFF"] = df["yacc_RAW_IMU"] - df["yacc_SCALED_IMU2"]
+    df["zacc_IMU_DIFF"] = df["zacc_RAW_IMU"] - df["zacc_SCALED_IMU2"]
+    df["xgyro_IMU_DIFF"] = df["xgyro_RAW_IMU"] - df["xgyro_SCALED_IMU2"]
+    df["ygyro_IMU_DIFF"] = df["ygyro_RAW_IMU"] - df["ygyro_SCALED_IMU2"]
+    df["zgyro_IMU_DIFF"] = df["zgyro_RAW_IMU"] - df["zgyro_SCALED_IMU2"]
+    df["xmag_IMU_DIFF"] = df["xmag_RAW_IMU"] - df["xmag_SCALED_IMU2"]
+    df["ymag_IMU_DIFF"] = df["ymag_RAW_IMU"] - df["ymag_SCALED_IMU2"]
+    df["zmag_IMU_DIFF"] = df["zmag_RAW_IMU"] - df["zmag_SCALED_IMU2"]
+
+    df.drop(
+        [
+            "roll_AHRS2",
+            "roll_AHRS3",
+            "pitch_AHRS2",
+            "pitch_AHRS3",
+            "yaw_AHRS2",
+            "yaw_AHRS3",
+            "xacc_RAW_IMU",
+            "xacc_SCALED_IMU2",
+            "yacc_RAW_IMU",
+            "yacc_SCALED_IMU2",
+            "zacc_RAW_IMU",
+            "zacc_SCALED_IMU2",
+            "xgyro_RAW_IMU",
+            "xgyro_SCALED_IMU2",
+            "ygyro_SCALED_IMU2",
+            "zgyro_RAW_IMU",
+            "zgyro_SCALED_IMU2",
+            "xmag_RAW_IMU",
+            "xmag_SCALED_IMU2",
+            "ymag_RAW_IMU",
+            "ymag_SCALED_IMU2",
+            "zmag_RAW_IMU",
+            "zmag_SCALED_IMU2",
+        ],
+        inplace=True,
+        axis=1,
+    )
+
+    return df
+
+
+def modeling(
+    which_model,
     X_train,
     y_train,
-    predict_all=False,
     X=None,
     y=None,
+    is_cross=False,
 ):
+    try:
+        if which_model == "ridge":
+            model = Ridge()
+
+        elif which_model == "xgb":
+            model = xgboost.XGBRegressor()
+
+        elif which_model == "mlp":
+            model = MLPRegressor()
+
+        elif which_model == "svr":
+            model = SVR()
+
+        else:
+            raise ValueError("check model name")
+
+    except ValueError:
+        print('ERROR!, check model name')
+
 
     """hyperparamerter tuning
     https://dining-developer.tistory.com/4
     """
 
-    # make xgb model
-    # xgb_model = xgboost.XGBRegressor(
-    #     n_estimators=100,
-    #     learning_rate=0.08,
-    #     gamma=0,
-    #     subsample=0.75,
-    #     colsample_bytree=1,
-    # )
-
-    xgb_model = xgboost.XGBRegressor()
-
-    param_grid = {
-        "booster": ["gbtree"],
-        "max_depth": [5, 6, 8],
-        "min_child_weight": [1, 3, 5],
-        "gamma": [0, 1, 2, 3],
-        "nthread": [4],
-        "colsample_bytree": [0.5, 0.8],
-        "colsample_bylevel": [0.9],
-        "n_estimators": [50],
-        "random_state": [2],
-    }
-
-    # gcv = GridSearchCV(
-    #     xgb_model,
-    #     cv=5,
-    #     scoring="neg_root_mean_squared_error",
-    #     n_jobs=4,
-    # )
-
-    # split
-    train_x, test_x, train_y, test_y = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-
-    # fitting
-    xgb_model.fit(train_x, train_y)
-    #
-    # print("best params", gcv.best_params_)  # best param
-    # print("best score", gcv.best_score_)  # best score
-
     # predict by dataset
-    if predict_all == False:
-        y_pred = cross_val_predict(xgb_model, X_train, y_train, cv=5)
-        y_true = y_train
-        result = X_train.copy()
+    """
+    N-N(8:2) 
+    N-F(ALL:ALL)
+    """
+    if is_cross == False:
+        # split
+        train_x, test_x, train_y, test_y = train_test_split(
+            X_train, y_train, test_size=0.2, random_state=42
+        )
+        # fitting by split dataset
+        model.fit(train_x, train_y)
 
-    elif predict_all == True:
-        y_pred = cross_val_predict(xgb_model, X, y, cv=5)
+        y_pred = model.predict(test_x)
+        y_true = test_y
+        result = test_x.copy()
+
+    elif is_cross == True:
+        # fitting by raw dataset
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X)
         y_true = y
         result = X.copy()
 
@@ -157,10 +261,12 @@ def xgb(
 if __name__ == "__main__":
 
     model = "xgb"
+    default_path = os.getcwd()  # /home/aiteam/son/pycharm
+    result_path = default_path + '/result'
 
-    output_metric = pd.DataFrame(
-        columns=["TARGET", "MAE", "MSE", "RMSE", "R_SQUARE"]
-    )
+    sys.exit(0)
+
+    output_metric = pd.DataFrame(columns=["TARGET", "MAE", "MSE", "RMSE", "R_SQUARE"])
     dicts = {}
 
     for tg in target_list:
@@ -170,29 +276,41 @@ if __name__ == "__main__":
         target = tg
 
         # read_csv
-        df = pd.read_csv(
+        df = pd.read_csv("/home/aiteam/son/AI-voucher-Smatii/data/normal_dataset.csv")
+        df_cross = pd.read_csv(
             "/home/aiteam/son/AI-voucher-Smatii/data/outlier_dataset.csv"
         )
+
+        df = df[col_list]
+        df = fix_columns(df)
+        df_cross = df_cross[col_list]
+        df_cross = fix_columns(df_cross)
 
         # make dataset
         x = df.drop(target, axis=1)
         y = df[target]
+        x_cross = df_cross.drop(target, axis=1)
+        y_cross = df_cross[target]
 
-        std_scaler = MinMaxScaler()
+        # scailing X
+        std_scaler = StandardScaler()
         std_scaler.fit(x)
-
         x_scale_trans = std_scaler.transform(x)
-
         scaled_x = pd.DataFrame(x_scale_trans, columns=x.columns)
 
-
-
-        # feature selection
-        # cols = feature_selection(x=x, y=y, k=k)
-        # x = x[cols]
+        scaled_x_cross = pd.DataFrame(
+            std_scaler.transform(x_cross), columns=x_cross.columns
+        )
 
         # run model
-        result, y_true, y_pred = xgb(X_train=scaled_x, y_train=y)
+        result, y_true, y_pred = modeling(
+            X_train=scaled_x,
+            y_train=y,
+            X=scaled_x_cross,
+            y=y_cross,
+            is_cross=False,
+            which_model="ridge",
+        )
 
         # make output prediction
         make_output_data(result, "xgb", target)
@@ -200,17 +318,15 @@ if __name__ == "__main__":
         # eval & make output eval
         mae, mse, rmse, r2 = evaluation(y_true, y_pred)
         dicts.update(
-            {
-                "TARGET": target,
-                "MAE": mae,
-                "MSE": mse,
-                "RMSE": rmse,
-                "R_SQUARE": r2,
-            }
+            {"TARGET": target, "MAE": mae, "MSE": mse, "RMSE": rmse, "R_SQUARE": r2}
         )
         output_metric = output_metric.append(dicts, True)
         print(output_metric)
 
         pass
 
-    output_metric.to_csv("SMT_out_XGB_prediction_mm_220111.csv", encoding="CP949", index=False)
+    output_metric.to_csv(
+        "result/220111/cross_test/prediction/SMT_out_XGB_prediction_rfe_std_cross_220113.csv",
+        encoding="CP949",
+        index=False,
+    )
